7c7407280aa819661d1383ef8ea87e3088202cfd
tomokp <tomoostkapelle@gmail.com>
2018-09-17 11:53:47 +0200

    3_done

--- javascript_les_oefening/assets/js/app.js
+++ javascript_les_oefening/assets/js/app.js
@@ -1,4 +1,4 @@
-const firstName = ("Tom"),
+/*const firstName = ("Tom"),
       lastName = ("de Bruijn"),
       age = (17),
       priceOfAnApple = (0.59),
@@ -10,4 +10,35 @@ let   totalPrice = (priceOfAnApple*numberOfApples);
 console.log ("Hallo, ik ben",firstName, lastName,"en ik ben",age,"jaar oud");
 console.log(totalPrice)
 console.log(`Totaalprijs is : ${totalPrice}`)
-console.log(replace)
+console.log(replace)*/
+
+let numberTest = (5); //vul hier je nummer in dat je wil testen of het even of oneven is
+
+
+if(numberTest%2==0) {
+      console.log("je nummer is even"); //hier wordt getest of het nummer even is, zo ja dan komt er te staan "je nummer is even".
+}
+
+else {
+      console.log("je nummer is oneven"); //als het nummer niet even wordt getest wordt in alle gevallen "je nummer is oneven" weergegeven.
+}
+
+
+
+const phrase = ("Programming is not so cool"), //dit is de basis zin
+replacePhrase = phrase.replace(" not", ""); //hierzo replace ik het woordje " not" (met de spatie ervoor) door niks waardoor het woordje not wordt weggehaald
+
+console.log(replacePhrase) //en hier laat ik dan het bewerkte zinnetje zien dmv console
+
+
+let number = (1400), //ik geef de waarde 1400 aan de variabele "number"
+    place = ("Naboo"); //ik geef de waarde "Naboo" aan de variabele "place"
+
+if (number==place) {
+      console.log("1400 is gelijk aan Naboo.") //ik kijk of de variabele "number" gelijk is aan de variabele "place", zo ja dan zegt de console "1400 is gelijk aan Naboo".
+}
+
+else {
+      console.log("1400 is niet gelijk aan Naboo.") //in elk ander geval dat "number" is niet gelijk aan "place" zegt de console; 1400 is niet geljk aan Naboo.
+}
+//dit is geen slimme vraag omdat je er niks aan hebt om een nummer met een woord zonder waarde te vergelijken sinds daar eigenlijk weinigs zinnigs uit komt.
