846d8794d25e65b619c8b0ecde1d35b94ea3977b
tomokp <tomoostkapelle@gmail.com>
2018-09-21 11:34:07 +0200

    vergelijken

---
+++ vergelijken/app.js
@@ -0,0 +1,32 @@
+let eersteWaarde = 5;
+let tweedeWaarde = 5.0;
+
+if (eersteWaarde==tweedeWaarde) {
+    console.log("dit is gelijk")
+}
+
+else {
+    console.log("dit is niet gelijk")
+}
+
+eersteWaarde = 10;
+tweedeWaarde = ("10");
+
+if (eersteWaarde==tweedeWaarde) {
+    console.log("dit is gelijk")
+}
+
+else {
+    console.log("dit is niet gelijk")
+}
+
+eersteWaarde = 10;
+tweedeWaarde = ("hoe werkt dit?")
+
+if (eersteWaarde==tweedeWaarde) {
+    console.log("dit is gelijk")
+}
+
+else {
+    console.log("dit is niet gelijk")
+}
