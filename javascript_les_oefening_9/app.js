71400eefc62dd89bad4ecaa2a16d0069dad10860
Tom de Bruijn <tomokp@users.noreply.github.com>
2018-10-09 13:06:16 +0200

    opdracht9 met commentaar

--- javascript_les_oefening_9/app.js
+++ javascript_les_oefening_9/app.js
@@ -1,63 +1,18 @@
-// window.addEventListener("load", init);
-
-// function init(){
-//     const button = document.getElementById("submit");
-//     button.addEventListener("click", clickEventHandler);
-// }
-
-
-// // const button = document.getElementById("submit");
-// // button.addEventListener("click", clickEventHandler);
-
-// // function clickEventHandler(event){
-// //     console.log(event);
-// // }
-// //var buttonId
-
-// // const container = document.getElementById("container");
-// // container.addEventListener("click", clickEventHandler);
-
-// // function clickEventHandler(event){
-// //     if (event.target.nodeName == "IMG") {
-// //         console.log(event.target.nodeName);
-// //     }
-// // }
-
-
-// const submitButton = document.getElementById("submit-btn");
-// submitButton.addEventListener("click", clickEventHandler);
-
-// function clickEventHandler(event){
-//     event.preventDefault();
-//     const inputText = document.getElementById("textInput").value;
-//     if(inputText == ""){
-//         document.getElementById("text").innerHTML = "je hebt niet echt wat nuttigs ingevuld";
-//     }
-//     else {
-//         document.getElementById("text").innerHTML = inputText;
-//     }
-// }
-
-
-
-
-
-
         const button = document.getElementById("submit");
-        button.addEventListener("click", clickEventHandler);
-        i = 0
+        button.addEventListener("click", clickEventHandler); //hier zeg ik dat als je op de knop drukt dat je de functie clickeventhandler start
+        i = 0                                                  //hier geef ik definieer ik i omdat ik die later nog wil gebruiken
 
 
-function clickEventHandler(event){
+function clickEventHandler(event){                         //dit is mijn functie clickeventhandler
         i= i+1;
         console.log(i);
-        document.getElementById("text").innerHTML = i;
-        if(i==10){
+        document.getElementById("text").innerHTML = i;     //elke keer als je op de knop drukt wordt i met 1 verhoogd en wordt het in het html bestand geschreven
+        if(i==10){                                      //als i bij 10 is geeft hij er een nieuwe class aan die zorgt dat de knop kleiner wordt
             var element = document.getElementById("submit");
             element.classList.remove("submitStyleOne");
             element.classList.add("submitStyleTwo");
         }
-        else if(i==20){
+        else if(i==20){                                 //als i bij de 20 is geeft hij er weer een andere class aan zodat het knopje nog kleiner wordt
             var element = document.getElementById("submit");
             element.classList.remove("submitStyleTwo");
             element.classList.add("submitStyleThree");
@@ -69,17 +24,16 @@ function clickEventHandler(event){
     
 
     const submitButton = document.getElementById("submit-btn");
-     submitButton.addEventListener("click", clickEventHandlerTwo);
-     var priceTotal = 0;
+     submitButton.addEventListener("click", clickEventHandlerTwo);   //als je op de button met id submit-btn klikt wordt de functie clickevenhandlertwo gestart
+     var priceTotal = 0;                                            //ik definieer hier pricetotal zodat ik die later kan gebruiken
     
-     function clickEventHandlerTwo(event){
-         event.preventDefault();
+     function clickEventHandlerTwo(event){                  //dit is mijn functie clickeventhandlertwo die voegt de ingevoerde waardes toe aan de html pagina en voegt er een knop aan toe om de waardes weer te verwijderen, 
+         event.preventDefault();                            //ook telt hij alle prijzen van de producten bij elkaar op en laat dit zien op de html pagina, als je op de knop verwijder klikt wordt dit ook weer van het totaal afgehaald
          var nameInput = document.getElementById("nameInput").value;
          console.log(nameInput);
-         //document.getElementById("productJs").innerHTML = nameInput;
+         
          var priceInput = document.getElementById("priceInput").value;
          console.log(priceInput);
-         //document.getElementById("productJs").innerHTML = priceInput;
 
          let table = document.getElementById("completeList");
         let row = table.insertRow(4);
@@ -122,6 +76,6 @@ function clickEventHandler(event){
            
             
         }
-        //priceTotalHtml
+        
          
      }
